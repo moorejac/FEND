7e6df605885382007e5e1d83f93c50667d8d7394
Jason Moore <moore.jac@outlook.com>
2018-07-02 16:49:11 -0400

    doc: clean up documentation

--- arcade_game/js/app.js
+++ arcade_game/js/app.js
@@ -46,9 +46,6 @@ class Enemy {
     }
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 class Player {
     constructor() {
@@ -78,8 +75,6 @@ class Player {
         };
     }
 
-    update() {}
-
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
@@ -92,39 +87,20 @@ class Player {
     handleInput(keyInput = "") {
         this.keyPress[keyInput]();
     }
-}/*
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+}
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-*/
-
-// Now instantiate your objects.
+// Now instantiate objects.
 // Place all enemy objects in an array called allEnemies
 enemy1 = new Enemy;
 enemy2 = new Enemy;
 enemy3 = new Enemy;
-const allEnemies = [enemy1, enemy2, enemy3];
-// Place the player object in a variable called player
+enemy4 = new Enemy;
+const allEnemies = [enemy1, enemy2, enemy3, enemy4];
 
+// Place the player object in a variable called player
 const player = new Player;
 
-
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
